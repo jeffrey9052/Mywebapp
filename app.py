ee72b5b200752e78786cf180d8d5dd7523c11285
Jeffrey Gonzalez <jeffrey9052@hotmail.com>
2020-06-28 01:14:04 -0400

    Primera carga

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('inicio.html')
+
+
+@app.route('/Servicios')
+def servicio():
+    return render_template('servicio.html')
+
+
+@app.route('/Servicios/Websites')
+def websites():
+    return render_template('website.html')
+
+
+@app.route('/Servicios/Redes')
+def redes():
+    return render_template('redes.html')
+
+
+@app.route('/Servicios/CCTV')
+def cctv():
+    return render_template('cctv.html')
+
+
+@app.route('/Servicios/Mantenimiento')
+def mantenimiento():
+    return render_template('mantenimiento.html')
+
+
+@app.route('/Acerca')
+def about():
+    return render_template('acerca.html')
+
+
+@app.route('/Contacto')
+def contacto():
+    return render_template('contacto.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
